dd78966dee5f3abd5a8598d781565bd46cdd8c96
fenitra <git.fandrianarisata@gmail.com>
2023-11-08 14:51:49 +0300

    v-for instruction

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
 <template>
     <div>
         <h1>{{ toUpperCaseString(message) }}</h1>
-        <h1>{{ age }}</h1>
+        <div v-if="isAdult">
+            <h2>Cette personne est adulte car son age est : {{ age }}</h2>
+        </div>
+        <div v-else>
+            <h2>Cette personne n'est pas une adulte car son age est : {{ age }}</h2>
+        </div>
+        <div v-show="isMety">
+            <h2>Tena mety le izy</h2>
+        </div>
+        <div class="list-users">
+            <ul>
+                <li v-for="(user, i) in users" :key="i">
+                    <div>Nom : {{ user.name }}</div>
+                    <div>
+                        Connecté: {{ user.isConnected ? 'en ligne' : 'Déconnecté' }}
+                    </div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 <script>
@@ -10,19 +28,43 @@
             return {
                 message : 'Bonjour tout le monde',
                 age : 25,
-                users : []
+                users : [
+                    {
+                        name : 'Fenitra',
+                        id : 1,
+                        isConnected : false
+                    },
+                    {
+                        name : 'Tojo',
+                        id : 2,
+                        isConnected : true
+                    },
+                    {
+                        name : 'Hary',
+                        id : 3,
+                        isConnected : true
+                    },
+                    {
+                        name : 'Andry Bihary',
+                        id : 4,
+                        isConnected : false
+                    }
+                ],
+                isMety : true
             }
         },
-        methods: {
-            toUpperCaseString(str){
-                return str.toUpperCase();
-            },
+        computed: {
             isAdult(){
                 if(this.age >= 18){
                     return true
                 }else{
                     return false
                 }
+            },
+        },
+        methods: {
+            toUpperCaseString(str){
+                return str.toUpperCase();
             },
             DisplayIfAdult(){
                 if(this.isAdult){
