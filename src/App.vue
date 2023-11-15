90373e3a05b8d6fda0345a825b854160c1d9455e
fenitra <git.fandrianarisata@gmail.com>
2023-11-15 13:44:44 +0300

    event

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,36 @@
 <template>
     <div>
-        <h1>Manontania : </h1>
-        <div>
-            <input type="text" v-model="question">
-        </div>
-        <div class="answer">{{ answer }}</div>
-        <div v-if="gif" class="image">
-            <img :src="gif" alt="">
-        </div>
-        <div>
-            <h1>Component global</h1>
-            <ComponentA text="Voici un text via component parent"/>
-        </div>
-        <div>
-            <h1>Deep props reactive</h1>
-            <input type="text" v-model="depMsg">
-        </div>
+       <button @click="afficherBonjour">Bonjour</button>
+        <button @click="afficherBonjourArg($event, 'RAsoa')">Bonjour iiii!!!</button>
+        <button @click.prevent="afficherBonjourArg($event, name)">Bonjour data</button>
+        <form action="">
+            <input type="text">
+            <button @click.prevent="afficherBonjourArg($event, name)">Bonjour data</button>
+        </form>
     </div>
 </template>
 <script>
 import { computed } from 'vue'
     export default {
-        // provide : {
-        //     messageApp : 'Hello from App parent',
-        //     arahaba : 'manahoana ee!!!'
-        // }, // modeo simple
-        data(){
+        /*
+            nom d'evènement : @click, @submit, @input (keyup, keydown), @keyup, @mouseenter, @mouseleave
+        */
+       data(){
             return {
-                question : '',
-                answer : 'asio question mark ? :-) ',
-                loading : false,
-                gif : null,
-                depMsg : ''
+                longueur : 15,
+                name : 'Andry'
             }
-        },
-        provide(){
-            return {
-                messageApp : 'Hello from App parent',
-                arahaba : 'manahoana ee!!!',
-                depMsg : computed(() => this.depMsg)
+       },
+       methods:{
+            afficherBonjour(){
+                alert('Bonjour tout le monde')
+            },
+            afficherBonjourArg(event, name){
+
+                alert('bonjour ' + name)
+                //event.preventDefault();
+                
             }
-        },
-        watch : {
-            question(newQuestion){
-                if(newQuestion.includes('?')){
-                    this.getAnswer()
-                }
-            }
-        },
-        methods: {
-            async getAnswer(){
-                this.loading = true;
-                this.answer = "Aoka aloha an ... :D";
-                try {
-                    const  res = await fetch('https://yesno.wtf/api');
-                    const _dataRes = await res.json()
-                    this.answer = _dataRes.answer
-                    this.gif = _dataRes.image
-                    
-                } catch (error) {
-                    this.answer = 'Error! tsy misy valiny yes or no'
-                } finally{
-                    this.loading = false
-                }
-            }
-        },
-        
+       }
     }
 </script>
